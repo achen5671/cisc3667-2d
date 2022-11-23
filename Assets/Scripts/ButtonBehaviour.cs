a80d700158da7dd92cfef525c4dd25eaed5b4e93
Michael Otasowie <michaelotasowie@Michaels-MBP.home>
2022-11-22 19:35:53 -0500

    Made it possible for the player to type their names once in the main menu and the game would remeber your name until you change it in the main menu again.

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -7,10 +7,17 @@ using UnityEngine.UI;
 public class ButtonBehaviour : MonoBehaviour
 {
      [SerializeField] InputField playerNameInput;
+     [SerializeField] string s;
      
     // Start is called before the first frame update
     void Start()
     {
+        s = PersistentData.Instance.GetName();
+
+        if (s != null)
+        {
+            playerNameInput.text = s;
+        }
         
     }
 
@@ -23,13 +30,15 @@ public class ButtonBehaviour : MonoBehaviour
     
     public void MainMenu()
     {
+         s = playerNameInput.text;
+        PersistentData.Instance.SetName(s);
         SceneManager.LoadScene("Menu");
     }
 
     public void PlayGame()
     {
         // This is to set the name in the PersistentData so that it follows along throughout the game
-        string s = playerNameInput.text;
+        s = playerNameInput.text;
         PersistentData.Instance.SetName(s);
 
         SceneManager.LoadScene("Start");
@@ -37,13 +46,15 @@ public class ButtonBehaviour : MonoBehaviour
 
     public  void HowToPlay()
     {
-        
+         s = playerNameInput.text;
+        PersistentData.Instance.SetName(s);
         SceneManager.LoadScene("How To Play");
     }
 
     public  void Settings()
     {
-        
+         s = playerNameInput.text;
+        PersistentData.Instance.SetName(s);
         SceneManager.LoadScene("Settings");
     }
 
