b4ec0e62aacea4501d709884badc920c324d51cf
BCMichaelBC <103379684+BCMichaelBC@users.noreply.github.com>
2022-12-03 16:14:57 -0500

    Score System & Random Chest Spawn (#18)
    * add score for correct conjugation
    * get random conjugations. remove unused file
    * made it so the chest gets destroyed after the player gets it right.
    * Added Wall Collision to Level 1
    * Made it so chest chests can randomly spawn in connected to the canvas’s dialog box and dialog text. Turned a bug that wouldn’t go away into a feature, the feature is now a welcome dialog box. Add the ChestSpawner.cs script to help spawn the chest, this is located in a game object called spawner.
    
    Co-authored-by: Andy Chen <44120813+achen5671@users.noreply.github.com>
    Co-authored-by: Michael Otasowie <michaelotasowie@Michaels-MacBook-Pro.local>
    Co-authored-by: NezuruiArt <ktrane9@gmail.com>
    Co-authored-by: Michael Otasowie <michaelotasowie@Michaels-MBP.home>

--- Assets/Scripts/README.md
+++ Assets/Scripts/README.md
@@ -1,1 +1,3 @@
 All scences script will be here for now. More targeted/specfics scripts that are for prefabs will appear in there respective folders e.g Player Folder -> Player script.
+
+verbs are manually added but are chosen randomly
--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+
 
 public class Sign : MonoBehaviour
 {
@@ -9,10 +11,33 @@ public class Sign : MonoBehaviour
     public Text dialogText;
     public string dialog;
     public bool playerInRange;
+    static System.Random random = new System.Random();
+    public bool isChest = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (isChest) {
+            // var _dialogText = GetComponent<Sign>().transform.GetChild(0).GetComponent<Text>();
+            // var _dialogBox = GameObject.Find("Canvas").GetComponent<Transform>().Find("dialog box").GetComponent<GameObject>();
+
+            // dialogBox = _dialogBox;
+            // dialogText = _dialogText;
+            // dialog = dialogText.text;
+            dialogBox = GameObject.Find("dialog box");
+            dialogText = GameObject.FindGameObjectWithTag("DialogText").GetComponent<Text>();
+            dialog = dialogText.text;
+            // If it's a chest, randomly give dialog a verb from the list
+            GetRandomConjugatedVerb();
+        }
+    }
+
+    // The greeting dialog box needs to get off the screen this script will do that after 3 seconds
+    IEnumerator Start()
+    {
+        dialogText.text = "Welcome to our game!! " + PersistentData.Instance.GetName() + " Have Fun!!";
+        yield return new WaitForSeconds(3f);
+        dialogBox.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,7 +56,28 @@ public class Sign : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")){
             playerInRange = true;
+
+            PickUp verbSign = other.GetComponent<PickUp>();
+            if (verbSign != null) {
+                if(CheckConjugation(dialog, verbSign.GetItemHoldingDialog())){
+                    // add score
+                    ScoreKeeper.AddScore();
+                    // todo: play sound
+                    Debug.Log("SCORE!");
+                    // Destroy(verbSign.GetItem());
+                    // Destroy(gameObject);
+                    Destroy(verbSign.itemHolding);
+                    // TODO: destory chest
+                }else{
+                    ScoreKeeper.MinusScore();
+                    // todo: play sound
+                    Debug.Log("WRONG!");
+                    Destroy(verbSign.itemHolding);
+
+                }
+            } 
         }
+        
     }
 
     private void OnTriggerExit2D(Collider2D other) {
@@ -40,4 +86,39 @@ public class Sign : MonoBehaviour
             dialogBox.SetActive(false);
         }
     }
+
+    public string GetDialogText() {
+        if (!playerInRange) return "";
+        return dialog;
+    }
+
+    // ### CONJUGATION CHECK
+    // This should return a list 
+    public List<string> GetConjugationValues(string pronoun) {
+        string values = PlayerPrefs.GetString(pronoun);
+
+        List<string> tokens = values.Split(", ").ToList();
+        return tokens;
+    }
+
+    // todo: Get pronoun from text in the sign when user press 'q'
+    // word is the conjugated verb in the chest
+    public bool CheckConjugation(string pronoun, string word) {
+        List<string> conjugations = GetConjugationValues(pronoun);
+        return conjugations.Contains(word);
+    }
+
+    public void GetRandomConjugatedVerb() {
+        string[] pronouns = {"yo", "tu", "el/ella/usted", "nosotros", "ellos/ellas/ustedes"};
+
+        int value = random.Next(pronouns.Length);
+        string pronoun = pronouns[value];
+
+        List<string> conjugations = GetConjugationValues(pronoun);
+        int rand = random.Next(conjugations.Count);
+
+        // Debug.Log(conjugations[rand]);
+        dialog = conjugations[rand];
+    }
+
 }
