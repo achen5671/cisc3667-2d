9e1622619b4f44129bf4cd31169a83c16dd55931
Andy Chen <44120813+achen5671@users.noreply.github.com>
2022-12-06 09:06:09 -0600

    bug: player will not flip correctly with animations

--- Assets/Characters/Player/PlayerMovement.cs
+++ Assets/Characters/Player/PlayerMovement.cs
@@ -5,98 +5,101 @@ using UnityEngine;
 // Takes and handles input and movement for a player character
 public class PlayerMovement : MonoBehaviour
 {
-   public float speed;
-   private Rigidbody2D myRigidBody;
-   private Vector3 change;
-   //private Vector3 motion;
-   private Animator animator;
-   const int idle = 0;
-   const int walk_down = 1;
-   const int walk_up = 2;
-   const int walk_side = 3;
-   public bool facingRight = true;
-   
-   void Start() {
-     myRigidBody = GetComponent<Rigidbody2D>();
-     
-     if(animator == null) {
-          animator = GetComponent<Animator>();
-     }
+  public float speed;
+  private Rigidbody2D myRigidBody;
+  private Vector3 change;
+  //private Vector3 motion;
+  private Animator animator;
+  const int idle = 0;
+  const int walk_down = 1;
+  const int walk_up = 2;
+  const int walk_side = 3;
+  public bool isFacingRight = true;
+  
+  void Start() {
+    myRigidBody = GetComponent<Rigidbody2D>();
+    
+    if(animator == null) {
+        animator = GetComponent<Animator>();
+    }
 
-   }
+  }
 
-   void Update() {
-        change = Vector3.zero;
-        change.x = Input.GetAxisRaw("Horizontal");
-        change.y = Input.GetAxisRaw("Vertical");
+  void Update() {
+      change = Vector3.zero;
+      change.x = Input.GetAxisRaw("Horizontal");
+      change.y = Input.GetAxisRaw("Vertical");
+  }
 
-        //animate();
+  void FixedUpdate() {
+    MoveCharacter();
+    //animate();
 
-        /*if (change != Vector3.zero) {
-          MoveCharacter(); 
-        }
+    /*if (change != Vector3.zero) {
+      MoveCharacter(); 
+    }
 
-        else {
-          animate(motion);
-          animator.SetInteger("Motion", idle);
-        }*/
+    else {
+      animate(motion);
+      animator.SetInteger("Motion", idle);
+    }*/
 
-        if(Input.GetKey("down")) {
-          animator.SetInteger("Motion", walk_down); //Play Walk Down Animation
-          MoveCharacter(); 
-        }
+    if(Input.GetKey("down")) {
+      animator.SetInteger("Motion", walk_down); //Play Walk Down Animation
+    }
 
-        else if(Input.GetKey("up")) {
-          animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
-          MoveCharacter(); 
-        }
-        
-        else if(Input.GetKey("right")) {
-          //if(facingRight == true)
-          animator.SetInteger("Motion", walk_side); //Play Walk Side Animation
-          MoveCharacter(); 
-        }
+    else if(Input.GetKey("up")) {
+      animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
+    }
 
-        else if(Input.GetKey("left")) {
-          transform.Rotate(new Vector2(0, 180));
-          animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
-          MoveCharacter(); 
-        }
+    // note: commenting this out for now because it's not working properly
+    // else if(change.x < 0 && isFacingRight || change.x > 0 && !isFacingRight) {
+    //   animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
+    //   Flip();
+    // }
 
-        else {
-          animator.SetInteger("Motion", idle); //Play Idle Animation
-        }
-   }
+    else {
+      animator.SetInteger("Motion", idle); //Play Idle Animation
+    }
+  }
 
-   void MoveCharacter() {
-        myRigidBody.MovePosition(transform.position + change * speed * Time.fixedDeltaTime); 
-   }
+  void MoveCharacter() {
+      myRigidBody.MovePosition(transform.position + change * speed * Time.fixedDeltaTime); 
+  }
 
-   /*void animate() {
-     switch(change) {
-          case change.y < 0:
-               animator.SetInteger("Motion", walk_down); //Play Walk Down Animation
-               MoveCharacter(); 
-               break;
+  // flip player facing direction
+  // void Flip()
+	// {
+	// 	transform.Rotate(0, 180, 0);
+  //   isFacingRight = !isFacingRight; 
+	// }
 
-          case change.y > 0:
-               animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
-               MoveCharacter(); 
-               break;
+  // unsed
+  /*void animate() {
+    switch(change) {
+        case change.y < 0:
+              animator.SetInteger("Motion", walk_down); //Play Walk Down Animation
+              MoveCharacter(); 
+              break;
 
-          case change.x > 0:
-               animator.SetInteger("Motion", walk_side); //Play Walk Side Animation
-               MoveCharacter(); 
-               break;
+        case change.y > 0:
+              animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
+              MoveCharacter(); 
+              break;
 
-          case change.x < 0:
-               animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
-               MoveCharacter(); 
-               break;
+        case change.x > 0:
+              animator.SetInteger("Motion", walk_side); //Play Walk Side Animation
+              MoveCharacter(); 
+              break;
 
-          default:
-               animator.SetInteger("Motion", idle); //Play Walk Down Animation
-               break;
-     }
-   }*/
+        case change.x < 0:
+              animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
+              MoveCharacter(); 
+              break;
+
+        default:
+              animator.SetInteger("Motion", idle); //Play Walk Down Animation
+              break;
+    }
+  }*/
 }
