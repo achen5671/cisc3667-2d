dc6ead7eb9ebe70bda5fed65455b994899189cd8
BCMichaelBC <103379684+BCMichaelBC@users.noreply.github.com>
2022-12-07 21:00:04 -0500

    Michael cheatsheet  (#24)
    * Made a new scene, 'Cheat sheet', also created tags for things related to the timer
    
    * added buttons to go to cheat sheet and back to game. Theres an bug where the timer keeps reseting as they move between scenes like giving them extra time
    
    * Fixed the bug now the time will continue throughout the scene from game to Cheat Sheet. i also made the Timer.cs file work like persistiant data in the context that it will not be destroyed on the next scene but after the times up signal it will destroy it self. Fixed a bug in the end scene where you can press the play again button
    
    * The timer script was causing dialog box issues because I wanted to keep it while it switches from cheat sheet to game and vise versa but all it did was cause bugs with the chest so I removed it from script. Added a way to get to cheat sheet scene without timer so the player can learn a little before playing. Added a lateUpdate to buttone behavior that checks to see if the scene is the cheat sheet scean and if the timer is active if so the timer will display if false the timer will not display so the player can study and take their time ( we can remove this later if you guys want). also made it so that if you started the game and left, for example you pressed the main menu button the timer is still going and youll be forced to go to end scene. This may causes problems if it does chekc timer.cs lines 59 - 63
    
    * little tweak, added Stopcoroutine() to see if that works in lines 59-64
    
    * added cheat sheet image
    
    Co-authored-by: Michael Otasowie <michaelotasowie@Michaels-MacBook-Pro.local>
    Co-authored-by: Andy Chen <44120813+achen5671@users.noreply.github.com>

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,14 +10,33 @@ using UnityEngine.SceneManagement;
 // Added a IEnumerator to allow a delay between changing to end scene 
 public class Timer : MonoBehaviour
 {
+
+    public static Timer onGoingTime;
+
     bool startTimer = false;
     public Slider timerSlider;
     public Text timerText;
     public float gameTime;
     public bool stopTimer;
     private IEnumerator coroutine;
+    public float time;
 
     public GameObject timesUpText;
+
+    private void Awake()
+    {
+        assign();
+        if (onGoingTime == null)
+        {
+            DontDestroyOnLoad(this);
+            onGoingTime = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+       
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +49,26 @@ public class Timer : MonoBehaviour
         
     }
 
+    void FixedUpdate()
+    {
+        if (timerSlider == null)
+        {
+            assign();
+
+        }
+        if(gameTime <= 0)
+        {
+            coroutine = endOfGame(5.0F);
+            StartCoroutine(coroutine);
+            StopCoroutine(coroutine);
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
         if (startTimer) {
-            float time = gameTime - Time.timeSinceLevelLoad;
+            time = gameTime - Time.timeSinceLevelLoad;
 
             int minutes = Mathf.FloorToInt(time/60);
             int seconds = Mathf.FloorToInt(time - minutes * 60f);
@@ -65,5 +99,16 @@ public class Timer : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene("EndScene");
         ScoreKeeper.ResetScore();
+        Destroy(gameObject);
+    }
+
+    void assign()
+    {
+        timerSlider = GameObject.FindGameObjectWithTag("TimesUpSlider").GetComponent<Slider>();
+        timerText = GameObject.Find("TimerText").GetComponent<Text>();
+        timesUpText = GameObject.FindGameObjectWithTag("TimesUpText");
+        timerSlider.maxValue = gameTime;
+        timerSlider.value = gameTime;
+        timesUpText.SetActive(false);
     }
 }
